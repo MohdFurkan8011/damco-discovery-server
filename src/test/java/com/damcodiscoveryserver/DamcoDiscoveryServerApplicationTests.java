543cb5a9086248c302a93efb37a6f7ebd414a61f
MohdFurkan <mfs0786siwal@gmail.com>
2021-07-25 14:49:01 +0530

    DAMCO-DISCOVERY-SERVER

---
+++ src/test/java/com/damcodiscoveryserver/DamcoDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damcodiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamcoDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
